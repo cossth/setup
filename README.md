31f565a41a96026390f3c41bc57abef87965807a
shubham <Shubham>
2020-01-25 15:06:13 +0530

    Add readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # setup-device
+enter this command 
+`curl -s https://setup.cossth.com/ | sudo bash`
--- init.sh
+++ init.sh
@@ -1,1 +1,12 @@
-curl -L https://github.com/cossth.keys >> ~/.ssh/authorized_keys
+echo "Remove Files if exist"
+rm githubkeys
+rm userkeys
+echo "Download keys from github" 
+curl -L https://github.com/cossth.keys > githubkeys
+echo "Move key"
+mv authorized_keys userkeys
+echo "Sort Keys"
+sort -mu githubkeys userkeys > authorized_keys
+echo "Cleanup"
+rm githubkeys
+rm userkeys
