b0652f06ddfaa28dd63bbe0aaa8f158640fc5e10
Shubham Sharma <cossth@gmail.com>
2021-03-23 13:05:14 +0530

    Create allow-ssh.sh

---
+++ allow-ssh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Remove Files if exist"
+rm githubkeys
+rm userkeys
+echo "Download keys from github" 
+curl -L https://github.com/cossth.keys > githubkeys
+echo "Move key"
+mv authorized_keys userkeys
+echo "Sort Keys"
+sort -mu githubkeys userkeys > authorized_keys
+echo "Cleanup"
+rm githubkeys
+rm userkeys
